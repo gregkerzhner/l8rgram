7632ec5c13e263492d1ec32a2e6aa881575a4989
Greg Kerzhner <guidoprincess@gmail.com>
2015-06-04 09:20:15 +0200

    Further gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,51 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect'),
   concat = require('gulp-concat'),
-  runSequence = require('run-sequence');
-  rename = require('gulp-rename');
-  clean = require('gulp-clean');
-  shell = require('gulp-shell');
-  uglify = require('gulp-uglify');
-  gzip = require('gulp-gzip');
-  minifyCSS = require('gulp-minify-css');
+  runSequence = require('run-sequence'),
+  rename = require('gulp-rename'),
+  clean = require('gulp-clean'),
+  shell = require('gulp-shell'),
+  uglify = require('gulp-uglify'),
+  gzip = require('gulp-gzip'),
+  minifyCSS = require('gulp-minify-css'),
+  template = require('gulp-template'),
+  sass = require('gulp-sass');
+
+
+gulp.task('appScripts', function() {
+  return gulp.src(['app/scripts/**/*.js'])
+    .pipe(concat('app-scripts.js'))
+    .pipe(gulp.dest('dist/scripts'))
+});
+
+
+gulp.task('appStyles', function () {  
+  return gulp.src('app/styles/**/*.scss')
+    .pipe(sass({
+
+    }))
+    .pipe(concat('app-styles.css'))
+    .pipe(gulp.dest('dist/styles'))
+});
+
+gulp.task('indexDev', function() {
+  return gulp.src('app/_index.html')
+    .pipe(template({env: "dev"}))
+    .pipe(rename(function (path) {
+      path.basename = path.basename.slice(1);
+    }))
+    .pipe(gulp.dest('dist'));
+});
 
 
 gulp.task('watch', function(){
   gulp.watch('app/scripts/**/*.js', ['appScripts']);
-  gulp.watch('app/scripts/**/*.tpl.html', ['templates']);
-  gulp.watch('app/styles/**/*.less', ['appStyles']);
+  gulp.watch('app/styles/**/*.scss', ['appStyles']);
   gulp.watch('app/_index.html', ['index-dev']);
 })
 
 gulp.task('default', function(){
-  runSequence('watch', function() {
+  runSequence('appScripts', 'appStyles','indexDev',  'watch', function() {
     console.log('Dev started');
   });
 });
