44190ef229ad7adceaf00d67888927df54655105
Greg Kerzhner <guidoprincess@gmail.com>
2015-06-04 11:11:23 +0200

    Fixing up browserify tasks

--- gulpfile.js
+++ gulpfile.js
@@ -18,9 +18,17 @@ var gulp = require('gulp'),
   reactify = require('reactify'),
   streamify = require('gulp-streamify');
 
+var _ = require('lodash');
+var fs = require('fs');
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var bowerResolve = require('bower-resolve');
+var nodeResolve = require('resolve');
+
 
 var vendorJs = [
-  'vendor/react/react.js'
+  'app/vendor/react/react.js'
 ]
 
 var vendorStyles = [
@@ -79,21 +87,120 @@ gulp.task('webserver', function() {
 });
 
 
-/*
 
-var path = {
-  HTML: 'src/index.html',
-  MINIFIED_OUT: 'build.min.js',
-  OUT: 'build.js',
-  DEST: 'dist',
-  DEST_BUILD: 'dist/build',
-  DEST_SRC: 'dist/src',
-  ENTRY_POINT: './src/js/App.js'
-};
-*/
+var production = (process.env.NODE_ENV === 'production');
+
+
+gulp.task('build-vendor', function () {
+
+  // this task will go through ./bower.json and
+  // uses bower-resolve to resolve its full path.
+  // the full path will then be added to the bundle using require()
+
+  var b = browserify({
+    // generate source maps in non-production environment
+    debug: !production
+  });
+
+  // get all bower components ids and use 'bower-resolve' to resolve
+  // the ids to their full path, which we need for require()
+  getBowerPackageIds().forEach(function (id) {
+
+    var resolvedPath = bowerResolve.fastReadSync(id);
+
+    b.require(resolvedPath, {
+
+      // exposes the package id, so that we can require() from our code.
+      // for eg:
+      // require('./vendor/angular/angular.js', {expose: 'angular'}) enables require('angular');
+      // for more information: https://github.com/substack/node-browserify#brequirefile-opts
+      expose: id
+
+    });
+  });
+
+  // do the similar thing, but for npm-managed modules.
+  // resolve path using 'resolve' module
+  getNPMPackageIds().forEach(function (id) {
+    b.require(nodeResolve.sync(id), { expose: id });
+  });
+
+  var stream = b.bundle().pipe(source('vendor-scripts.js'));
+
+  // pipe additional tasks here (for eg: minifying / uglifying, etc)
+  // remember to turn off name-mangling if needed when uglifying
+
+  stream.pipe(gulp.dest('./dist/scripts'));
+
+  return stream;
+});
+
+gulp.task('build-app', function () {
+
+  var b = browserify('app/scripts/app.js', {
+    // generate source maps in non-production environment
+    debug: !production
+  });
+
+  // mark vendor libraries defined in bower.json as an external library,
+  // so that it does not get bundled with app.js.
+  // instead, we will load vendor libraries from vendor.js bundle
+  getBowerPackageIds().forEach(function (lib) {
+    b.external(lib);
+  });
+
+
+  // do the similar thing, but for npm-managed modules.
+  // resolve path using 'resolve' module
+  getNPMPackageIds().forEach(function (id) {
+    b.external(id);
+  });
+
+  var stream = b.bundle().pipe(source('app.js'));
+
+  // pipe additional tasks here (for eg: minifying / uglifying, etc)
+  // remember to turn off name-mangling if needed when uglifying
+
+  stream.pipe(gulp.dest('./dist/scripts'));
+
+  return stream;
+
+});
+
+/**
+ * Helper function(s)
+ */
+
+function getBowerPackageIds() {
+  // read bower.json and get dependencies' package ids
+  var bowerManifest = {};
+  try {
+    bowerManifest = require('./bower.json');
+  } catch (e) {
+    // does not have a bower.json manifest
+  }
+  return _.keys(bowerManifest.dependencies) || [];
+
+}
+
+
+function getNPMPackageIds() {
+  // read package.json and get dependencies' package ids
+  var packageManifest = {};
+  try {
+    packageManifest = require('./package.json');
+  } catch (e) {
+    // does not have a package.json manifest
+  }
+  return _.keys(packageManifest.dependencies) || [];
+
+}
+
 
 gulp.task('watch', function(){
-  //gulp.watch('app/scripts/**/*.js', ['appScripts']);
+  gulp.watch('app/scripts/**/*.js', ['build-app']);
+
+  /*
   var watcher  = watchify(browserify({
     entries: ['app/scripts/app.js'],
     transform: [reactify],
@@ -111,7 +218,7 @@ gulp.task('watch', function(){
     .pipe(source('app-scripts.js'))
     .pipe(gulp.dest('dist'));
 
-
+  */
 
 
   gulp.watch('app/styles/**/*.scss', ['appStyles']);
@@ -119,7 +226,7 @@ gulp.task('watch', function(){
 })
 
 gulp.task('default', function(){
-  runSequence('clean', 'vendorScripts', 'appStyles', 'vendorStyles', 'indexDev', 'watch', 'webserver',function() {
+  runSequence('clean', 'build-vendor', 'build-app', 'appStyles', 'vendorStyles', 'indexDev', 'watch', 'webserver',function() {
     console.log('Dev started');
   });
 });
