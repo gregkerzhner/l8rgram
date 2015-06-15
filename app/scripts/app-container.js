145c8d5eae0a797b7b18beb17249cd6e6e26226e
Greg Kerzhner <guidoprincess@gmail.com>
2015-06-15 12:41:53 +0200

    Navbar

--- app/scripts/app-container.js
+++ app/scripts/app-container.js
@@ -1,6 +1,9 @@
 var React = require('react');
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
+var Navbar = require('react-bootstrap').Navbar;
+var Nav = require('react-bootstrap').Nav;
+var NavItem = require('react-bootstrap').NavItem;
 
 var AppContainer = React.createClass({
   getInitialState: function(){
@@ -23,9 +26,15 @@ var AppContainer = React.createClass({
   },
   render: function(){
     return (
-      <div className="container-fluid">
-        heheheh
-        <RouteHandler/>
+      <div>
+        <Navbar brand='l8rgram' inverse toggleNavKey={0}>
+          <Nav right eventKey={0}> {/* This is the eventKey referenced */}
+            <NavItem eventKey={1} href='#'>Link</NavItem>
+          </Nav>
+        </Navbar>
+        <div className="container-fluid">
+          <RouteHandler/>
+        </div>
       </div>
     )
   }
